7ead0aa1577afc59e970435d11f660a32ebca2e2
Manu <emmanuel_giner_jr@hotmail.fr>
2014-10-24 16:18:22 +0200

    First test

---
+++ create.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rst2html input.rst > output.html
+declare -i BODY1
+declare -i BODY2
+BODY1=$(grep -n body output.html | cut -d ':' -f 1 | head -1)
+BODY2=$(grep -n body output.html | cut -d ':' -f 1 | tail -1)
+
+BODY1+=1
+BODY2=$BODY2-$BODY1-1
+
+TEXT=$(tail -n +$BODY1 output.html | head -n $BODY2)
+rm output.html
+
+
+declare -i INPUT
+declare -i INPUT1
+declare -i INPUT2
+INPUT1=$(grep -n INPUT index_template | cut -d ':' -f 1 | head -1)
+INPUT1+=-1
+INPUT2=$INPUT1+2
+
+function output ()
+{
+head -n +$INPUT1 index_template 
+echo $TEXT
+tail -n +$INPUT2 index_template 
+}
+
+output > index.html
+
+
---
+++ input.rst
@@ -0,0 +1,19 @@
+Publications
+============
+
+* `Papier 1 : <http://pouet.org>`_
+* `Papier 2 : <http://pouet.org>`_
+
+Oral presentations
+==================
+
+sous-titre
+----------
+
+pouet
+
+Software
+========
+
+* `Quantum package <http://github.com/LCPQ/quantum_package>`_
+
